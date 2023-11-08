050a7e99adc029b4ee226b840e783850bb0c0a84
Jaztin <110211588+bjaztin@users.noreply.github.com>
2023-11-08 23:33:10 +0800

    Update firebaseAuth.js

--- __mocks__/firebaseAuth.js
+++ __mocks__/firebaseAuth.js
@@ -8,36 +8,5 @@ export const signInWithEmailAndPassword = jest
   .mockResolvedValue({ user: {} });
 
 
-// const auth = jest.createMockFromModule("@react-native-firebase/auth");
-
-// __mocks__/firebaseAuth.js
-
-// const auth = jest.requireActual("@react-native-firebase/auth");
-
-// // Mock the functions you need
-// auth.signInWithEmailAndPassword = async (email, password) => {
-//   if (email === "example@example.com" && password === "password") {
-//     return {
-//       user: {
-//         uid: "123",
-//         displayName: "John Doe",
-//       },
-//     };
-//   } else {
-//     throw new Error("Authentication failed");
-//   }
-// };
-
-// auth.signOut = async () => {
-//   return Promise.resolve();
-// };
-
-// auth.currentUser = () => ({
-//   uid: "123",
-//   displayName: "John Doe",
-// });
-
-// module.exports = auth;
-
 
 
